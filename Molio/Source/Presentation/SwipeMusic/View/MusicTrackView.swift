d268b2d2c6d316cb4d119cd2044b7d1a2531c7d8
Minsoo Kang <alstn38@naver.com>
2024-11-07 13:06:39 +0900

    feat: MusicTrackView UI 작업 (#2)

---
+++ Molio/Source/Presentation/SwipeMusic/View/MusicTrackView.swift
@@ -0,0 +1,104 @@
+import UIKit
+
+final class MusicTrackView: UIView {
+    
+    private let albumImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 22
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .systemPink
+        return imageView
+    }()
+    
+    private let songTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "APT." //TODO: 이후 삭제
+        label.font = .systemFont(ofSize: 48, weight: .bold) // TODO: 변경된 폰트 적용해야 함
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "로제 & Bruno Mars" //TODO: 이후 삭제
+        label.font = .systemFont(ofSize: 20, weight: .medium) // TODO: 변경된 폰트 적용해야 함
+        label.textColor = .white.withAlphaComponent(0.7)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let genreStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.alignment = .leading
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        setupShadow()
+        setupHierarchy()
+        setupConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupShadow()
+        setupHierarchy()
+        setupConstraint()
+    }
+    
+    private func setupShadow() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .zero
+        layer.shadowRadius = 26
+        layer.shadowOpacity = 0.36
+    }
+    
+    private func setupGenre(_ genres: [String]) {
+        genreStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        
+        genres.forEach { genre in
+            let genreTagView = MusicTagView(tagTitle: genre, backgroundColor: .black.withAlphaComponent(0.3))
+            genreStackView.addArrangedSubview(genreTagView)
+        }
+    }
+    
+    private func setupHierarchy() {
+        addSubview(albumImageView)
+        addSubview(songTitleLabel)
+        addSubview(artistNameLabel)
+        addSubview(genreStackView)
+    }
+    
+    private func setupConstraint() {
+        NSLayoutConstraint.activate([
+            albumImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            albumImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            albumImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            albumImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            songTitleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -121),
+            songTitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 28)
+        ])
+        
+        NSLayoutConstraint.activate([
+            artistNameLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -92),
+            artistNameLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 28)
+        ])
+        
+        NSLayoutConstraint.activate([
+            genreStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -34),
+            genreStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 28)
+        ])
+    }
+}
