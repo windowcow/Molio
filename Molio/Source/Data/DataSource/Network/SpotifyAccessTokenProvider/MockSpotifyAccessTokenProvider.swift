64b594fdb5fbb2884e4e4de6ad682466abe66d1e
windowcow <bzbzkang@gmail.com>
2024-11-07 17:58:21 +0900

    feat: MockSpotifyAccessTokenProvider 로직 구체화하기
    - 이걸 기반으로 구체적으로 DefaultSpotifyAccessTokenProvider를 작성할 예정

--- Molio/Source/Data/DataSource/Network/SpotifyAccessTokenProvider/MockSpotifyAccessTokenProvider.swift
+++ Molio/Source/Data/DataSource/Network/SpotifyAccessTokenProvider/MockSpotifyAccessTokenProvider.swift
@@ -1,6 +1,23 @@
 struct MockSpotifyAccessTokenProvider: SpotifyAccessTokenProvider {
+    // 이 부분은 바꾸지 말기, 시작 값이 ""이다
+    private var accessToken: String = ""
+    
     func getAccessToken() async -> String {
-        // 여기에 토큰을 넣어서 테스트할 수 있습니다.
+        if await isValid(token: accessToken) {
+            return accessToken
+        }
+        
+        return await fetchNewAccessToken()
+    }
+    
+    private func isValid(token: String) async -> Bool {
+        // 서버에서 토큰이 유효하지 검증하거나 만료 시간으로 검증하는 로직 작성
+        
+        return true
+    }
+    
+    private func fetchNewAccessToken() async -> String {
+        // 서버에서 토큰을 받아오는 로직
         return ""
     }
 }
