b4304b5aed965ae79650e18507f8db90885b9976
kybeen <rei980627@gmail.com>
2024-11-10 03:45:44 +0900

    feat: Spotify Authorization API 엔드포인트 정의 (#12)
    - Spotify Authorization 요청 API 엔트포인트 정의

---
+++ Molio/Source/Data/DataSource/Network/SpotifyTokenProvider/SpotifyAuthorizationAPI.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+enum SpotifyAuthorizationAPI {
+    case getAccessToken
+    
+    var description: String {
+        switch self {
+        case .getAccessToken: "access token 요청 엔드포인트"
+        }
+    }
+}
+
+extension SpotifyAuthorizationAPI: EndPoint {
+    var baseURL: URL { URL(string: "https://accounts.spotify.com/api")! }
+    
+    var path: String {
+        switch self {
+        case .getAccessToken:
+            let params: [String: String] = ["grant_type": "client_credentials"]
+            return makeFullPath(with: "", params: params)
+        }
+    }
+    
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .getAccessToken: return .post
+        }
+    }
+    
+    var headers: [String: String]? {
+        return [
+            Header.Authorization.field: Header.Authorization.value,
+            Header.ContentType.field: Header.ContentType.value
+        ]
+    }
+    
+    var body: Data? {
+        switch self {
+        case .getAccessToken: return nil
+        }
+    }
+}
+
+extension SpotifyAuthorizationAPI {
+    private func makeFullPath(with path: String, params: [String: String]) -> String {
+        return params.reduce(path + "?") { result, param in
+            result + "&\(param.key)=\(param.value)"
+        }
+    }
+    
+    enum Header {
+        enum Authorization {
+            static let field = "Authorization"
+            static var value: String {
+                // TODO: - Base64 인코딩
+                guard let clientID = Bundle.main.object(forInfoDictionaryKey: "SPOTIFY_CLIENT_ID"),
+                      let clientSecret = Bundle.main.object(forInfoDictionaryKey: "SPOTIFY_CLIENT_SECRET") else {
+                    return ""
+                }
+                return "Basic \(clientID):\(clientSecret)"
+            }
+        }
+        
+        enum ContentType {
+            static let field = "Content-Type"
+            static let value = "application/x-www-form-urlencoded"
+        }
+    }
+}
