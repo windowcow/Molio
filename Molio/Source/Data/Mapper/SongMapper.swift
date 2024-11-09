ae1ef5c820124fdd9ddadd0db6c33e3307191011
Minsoo Kang <alstn38@naver.com>
2024-11-10 02:08:50 +0900

    feat: SongMapper 구현 (#23)

---
+++ Molio/Source/Data/Mapper/SongMapper.swift
@@ -0,0 +1,24 @@
+import MusicKit
+
+struct SongMapper {
+    static func toDomain(_ song: Song?) -> Music? {
+        guard let song,
+              let isrc = song.isrc,
+              let previewAsset = song.previewAssets?.first?.url,
+              let artworkImageURL = song.artwork?.url(width: 430, height: 430),
+              let artworkBackgroundColor = CGColorMapper.toDomain(song.artwork?.backgroundColor),
+              let primaryTextColor = CGColorMapper.toDomain(song.artwork?.primaryTextColor) else {
+            return nil
+        }
+        
+        return Music(title: song.title,
+                     artistName: song.artistName,
+                     gerneNames: song.genreNames,
+                     isrc: isrc,
+                     previewAsset: previewAsset,
+                     artworkImageURL: artworkImageURL,
+                     artworkBackgroundColor: artworkBackgroundColor,
+                     primaryTextColor: primaryTextColor
+        )
+    }
+}
