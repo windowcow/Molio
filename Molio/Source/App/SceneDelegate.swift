2d6bec785c9e8f3bfaa104c09a0207b807ff1dcb
Park-kxng <coding.parkgy29@gmail.com>
2024-11-11 01:04:05 +0900

    feat: 앱 시작 시 오디오 세션 활성화 (#25)

--- Molio/Source/App/SceneDelegate.swift
+++ Molio/Source/App/SceneDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import AVFAudio
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -7,8 +8,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        configureAudioSession()
+        
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = UINavigationController(rootViewController: SwipeMusicViewController())
         window?.makeKeyAndVisible()
+    }
+    
+    // 오디션 세션 활성화
+    private func configureAudioSession() {
+        if !AVAudioSession.accessibilityActivate() {
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [])
+                try AVAudioSession.sharedInstance().setActive(true)
+            } catch {
+                print("Failed to set up audio session: \(error)") //  TODO: 에러 알림창으로 표시하기
+            }
+        }
     }
 }
