369352dc4da8c9c9701b7efeb36f196dde8b2675
Minsoo Kang <alstn38@naver.com>
2024-11-12 18:55:31 +0900

    feat: SwipeMusicViewController 관련 의존성 주입 (#23)

--- Molio/Source/App/SceneDelegate.swift
+++ Molio/Source/App/SceneDelegate.swift
@@ -7,8 +7,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        let mockSpotifyAPIService = MockSpotifyAPIService()
+        let defaultMusicKitService = DefaultMusicKitService()
+        let defaultMusicRepository = DefaultMusicRepository(
+            spotifyAPIService: mockSpotifyAPIService,
+            musicKitService: defaultMusicKitService
+        )
+        let defaultFetchMusicsUseCase = DefaultFetchMusicsUseCase(repository: defaultMusicRepository)
+        let swipeMusicViewModel = SwipeMusicViewModel(fetchMusicsUseCase: defaultFetchMusicsUseCase)
+        let swipeMusicViewController = SwipeMusicViewController(viewModel: swipeMusicViewModel)
+        
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: SwipeMusicViewController())
+        window?.rootViewController = UINavigationController(rootViewController: swipeMusicViewController)
         window?.makeKeyAndVisible()
     }
 }
