4d1e8c3bb620b72f234317dba3f91e8001d141c6
Minsoo Kang <alstn38@naver.com>
2024-11-14 18:45:05 +0900

    feat: button, swipe input-ouput 구현 (#40)

--- Molio/Source/Presentation/SwipeMusic/ViewModel/SwipeMusicViewModel.swift
+++ Molio/Source/Presentation/SwipeMusic/ViewModel/SwipeMusicViewModel.swift
@@ -5,12 +5,33 @@ import MusicKit
 final class SwipeMusicViewModel: InputOutputViewModel {
     struct Input {
         let viewDidLoad: AnyPublisher<Void, Never>
+        let musicCardDidChangeSwipe: AnyPublisher<CGFloat, Never>
+        let musicCardDidFinishSwipe: AnyPublisher<CGFloat, Never>
+        let likeButtonDidTap: AnyPublisher<Void, Never>
+        let dislikeButtonDidTap: AnyPublisher<Void, Never>
     }
     
     struct Output {
         let currentMusicTrack: AnyPublisher<SwipeMusicTrackModel, Never>
         let isLoading: AnyPublisher<Bool, Never> // TODO: 로딩 UI 구현 및 연결
+        let buttonHighlight: AnyPublisher<ButtonHighlight, Never>
+        let musicCardSwipeAnimation: AnyPublisher<SwipeDirection, Never>
         let error: AnyPublisher<String, Never> // TODO: Error에 따른 알림 UI 구현 및 연결
+    }
+    
+    enum SwipeDirection: CGFloat {
+        case left = -1.0
+        case right = 1.0
+        case none = 0
+    }
+    
+    struct ButtonHighlight {
+        let isLikeHighlighted: Bool
+        let isDislikeHighlighted: Bool
+    }
+    
+    var swipeThreshold: CGFloat {
+        return 200.0
     }
     
     private let musicPlayer = SwipeMusicPlayer()
@@ -19,6 +40,8 @@ final class SwipeMusicViewModel: InputOutputViewModel {
     private var musicsSubject = CurrentValueSubject<[RandomMusic], Never>([])
     private let currentMusicCardPublisher = PassthroughSubject<SwipeMusicTrackModel, Never>()
     private let isLoadingPublisher = PassthroughSubject<Bool, Never>()
+    private let buttonHighlightPublisher = PassthroughSubject<ButtonHighlight, Never>()
+    private let musicCardSwipeAnimationPublisher = PassthroughSubject<SwipeDirection, Never>()
     private let errorPublisher = PassthroughSubject<String, Never>()
     private var cancellables = Set<AnyCancellable>()
     
@@ -43,8 +66,63 @@ final class SwipeMusicViewModel: InputOutputViewModel {
             }
             .store(in: &cancellables)
         
+        input.musicCardDidChangeSwipe
+            .map { [weak self] translation -> ButtonHighlight in
+                guard let self else {
+                    return ButtonHighlight(isLikeHighlighted: false, isDislikeHighlighted: false)
+                }
+                return ButtonHighlight(isLikeHighlighted: translation > self.swipeThreshold,
+                                       isDislikeHighlighted: translation < -self.swipeThreshold
+                )
+            }
+            .sink { [weak self] buttonHighlight in
+                self?.buttonHighlightPublisher.send(buttonHighlight)
+            }
+            .store(in: &cancellables)
+        
+        input.musicCardDidFinishSwipe
+            .map { [weak self] translation -> SwipeDirection in
+                guard let self else { return .none }
+                if translation > self.swipeThreshold {
+                    // TODO: 노래 좋아요에 대한 처리 추가하기
+                    return .right
+                } else if translation < -self.swipeThreshold {
+                    // TODO: 노래 싫어요에 대한 처리 추가하기
+                    return .left
+                } else {
+                    return .none
+                }
+            }
+            .sink { [weak self] swipeDirection in
+                guard let self else { return }
+                self.musicCardSwipeAnimationPublisher.send(swipeDirection)
+                self.buttonHighlightPublisher.send(
+                    ButtonHighlight(isLikeHighlighted: false,
+                                    isDislikeHighlighted: false)
+                )
+            }
+            .store(in: &cancellables)
+        
+        input.likeButtonDidTap
+            .sink { [weak self] _ in
+                guard let self else { return }
+                // TODO: 노래 좋아요에 대한 처리 추가하기
+                self.musicCardSwipeAnimationPublisher.send(.right)
+            }
+            .store(in: &cancellables)
+        
+        input.dislikeButtonDidTap
+            .sink { [weak self] _ in
+                guard let self else { return }
+                // TODO: 노래 싫어요 대한 처리 추가하기
+                self.musicCardSwipeAnimationPublisher.send(.left)
+            }
+            .store(in: &cancellables)
+        
         return Output(currentMusicTrack: currentMusicCardPublisher.eraseToAnyPublisher(),
                       isLoading: isLoadingPublisher.eraseToAnyPublisher(),
+                      buttonHighlight: buttonHighlightPublisher.eraseToAnyPublisher(),
+                      musicCardSwipeAnimation: musicCardSwipeAnimationPublisher.eraseToAnyPublisher(),
                       error: errorPublisher.eraseToAnyPublisher()
         )
     }
