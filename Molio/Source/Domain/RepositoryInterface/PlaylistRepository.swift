de96a37428c353cd1e8c86140eb8415a60260771
Park-kxng <coding.parkgy29@gmail.com>
2024-11-17 00:48:31 +0900

    feat: PlaylistRepository에 playlistsPublisher 추가 (#39)

--- Molio/Source/Domain/RepositoryInterface/PlaylistRepository.swift
+++ Molio/Source/Domain/RepositoryInterface/PlaylistRepository.swift
@@ -1,6 +1,9 @@
 import Foundation
+import Combine
 
 protocol PlaylistRepository {
+    var playlistsPublisher: AnyPublisher<[PlaylistDTO], Never> { get }
+    
     func addMusic(isrc: String, to playlistName: String)
     func deleteMusic(isrc: String, in playlistName: String)
     func moveMusic(isrc: String, in playlistName: String, fromIndex: Int, toIndex: Int)
