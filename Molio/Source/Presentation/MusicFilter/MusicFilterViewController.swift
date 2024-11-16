4002cb853bfadb9e6d540d67e6b8cf66d5985d68
kybeen <rei980627@gmail.com>
2024-11-16 17:32:24 +0900

    feat: MusicFilterViewController 정의 (#36)
    - UIHostingController를 상속받는 뷰컨트롤러
    - 필터 편집 화면의 내비게이션 바 설정

---
+++ Molio/Source/Presentation/MusicFilter/MusicFilterViewController.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+final class MusicFilterViewController: UIHostingController<MusicFilterView> {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.molioSemiBold(text: "내 필터 편집", size: 17)
+        label.textColor = .white
+        return label
+    }()
+    
+    private let saveButtonItemLabel: UILabel = {
+        let label = UILabel()
+        label.molioRegular(text: "저장", size: 17)
+        label.textColor = .main
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = false
+        
+        // TODO: - 뒤로가기 버튼 텍스트 숨기기
+        
+        navigationItem.titleView = titleLabel
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: saveButtonItemLabel)
+    }
+}
