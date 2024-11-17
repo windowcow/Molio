b650e6bc9c82a9c133e7613c1fe7c4221ba4c38f
Park-kxng <coding.parkgy29@gmail.com>
2024-11-17 16:05:56 +0900

    refactor: PlaylistDTO -> PlaylistEntity로 변경 (#39)

--- Molio/Source/Domain/RepositoryInterface/PlaylistRepository.swift
+++ Molio/Source/Domain/RepositoryInterface/PlaylistRepository.swift
@@ -2,14 +2,14 @@ import Foundation
 import Combine
 
 protocol PlaylistRepository {
-    var playlistsPublisher: AnyPublisher<[PlaylistDTO], Never> { get }
+    var playlistsPublisher: AnyPublisher<[PlaylistEntity], Never> { get }
     
     func addMusic(isrc: String, to playlistName: String)
     func deleteMusic(isrc: String, in playlistName: String)
     func moveMusic(isrc: String, in playlistName: String, fromIndex: Int, toIndex: Int)
 
-    func fetchPlaylists() -> [PlaylistDTO]?
+    func fetchPlaylists() -> [PlaylistEntity]?
     func saveNewPlaylist(_ playlistName: String)
     func deletePlaylist(_ playlistName: String)
-    func fetchPlaylist(for name: String) -> PlaylistDTO?
+    func fetchPlaylist(for name: String) -> PlaylistEntity?
 }
