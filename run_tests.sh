c9ba44e34acf4755dce99b9d69db57bdc2d4d688
kybeen <rei980627@gmail.com>
2024-11-20 01:09:46 +0900

    chore: destination 타겟 수정 (#68)
    - 깃허브 액션 가상머신 Runner 기준과 프로젝트 스펙에 맞춤

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCHEME='Molio'
-DESTINATION='platform=iOS Simulator,OS=18.0,name=iPhone 16 Pro'
+DESTINATION='platform=iOS Simulator,OS=17.0.1,name=iPhone 15 Pro'
 xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO'
