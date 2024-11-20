3dc18d7e9b5e006a07a4874b43e997664da4e8db
windowcow <bzbzkang@gmail.com>
2024-11-20 22:26:17 +0900

    feat: PublishCurrentPlaylistUseCase 복구 (#119)
    - PublishCurrentPlaylistUseCase를 async를 사용할 수 있도록 수정
    - 테스트 코드 작성

--- Molio/Source/Domain/UseCase/PublishCurrentPlaylistUseCase/DefaultPublishCurrentPlaylistUseCase.swift
+++ Molio/Source/Domain/UseCase/PublishCurrentPlaylistUseCase/DefaultPublishCurrentPlaylistUseCase.swift
@@ -1,26 +1,39 @@
 import Combine
 import Foundation
 
-// TODO: async 함수 반영
-//final class DefaultPublishCurrentPlaylistUseCase: PublishCurrentPlaylistUseCase {
-//    private let playlistRepository: any PlaylistRepository
-//    private let currentPlaylistRepository: any CurrentPlaylistRepository
-//    
-//    init(
-//        playlistRepository: any PlaylistRepository,
-//        currentPlaylistRepository: any CurrentPlaylistRepository
-//    ) {
-//        self.playlistRepository = playlistRepository
-//        self.currentPlaylistRepository = currentPlaylistRepository
-//    }
-//    
-//    func execute() -> AnyPublisher<MolioPlaylist?, Never>  {
-//        currentPlaylistRepository.currentPlaylistPublisher
-//            .flatMap { [weak self] playlistUUID in
-//                let molioPlaylist = self?.playlistRepository.fetchPlaylist(for: playlistUUID?.uuidString ?? "")
-//                
-//                return Just(molioPlaylist).eraseToAnyPublisher()
-//            }
-//            .eraseToAnyPublisher()
-//    }
-//}
+final class DefaultPublishCurrentPlaylistUseCase: PublishCurrentPlaylistUseCase {
+    private let playlistRepository: any PlaylistRepository
+    private let currentPlaylistRepository: any CurrentPlaylistRepository
+    
+    init(
+        playlistRepository: any PlaylistRepository,
+        currentPlaylistRepository: any CurrentPlaylistRepository
+    ) {
+        self.playlistRepository = playlistRepository
+        self.currentPlaylistRepository = currentPlaylistRepository
+    }
+    
+    func execute() -> AnyPublisher<MolioPlaylist?, Never>  {
+        currentPlaylistRepository.currentPlaylistPublisher
+            .flatMap {  playlistUUID in
+                return Future { promise in
+                    Task { [weak self] in
+                        guard
+                            let self,
+                            let playlistUUID else {
+                            promise(.success(nil))
+                            return                         }
+                        
+
+                        let playlist = try? await self.playlistRepository.fetchPlaylist(for: playlistUUID.uuidString)
+                        
+                        promise(.success(playlist))
+                    }
+                    
+                    return
+                }
+                .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+}
